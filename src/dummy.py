2a831f57e33c123856f2294ea477223120d01f32
nganhtu <60926756+nganhtu@users.noreply.github.com>
2024-06-16 14:23:07 +0700

    Implement auras and freeze aura

--- src/dummy.py
+++ src/dummy.py
@@ -1,13 +1,27 @@
 from icecream import ic
 
 from config import *
-import pygame as pg
+from genshin import *
 
 
 class Dummy():
     def __init__(self):
-        self.auras = []
         self.time = 0
+        self.auras = []
+        self.freeze_aura = None
+        self.freeze_aura_decay_speed = FREEZE_AURA_STARTING_DECAY_SPEED
+
+    def isFrozen(self):
+        return self.freeze_aura is not None
+
+    def update_freeze_params(self, dt):
+        if self.isFrozen():
+            self.freeze_aura_decay_speed = self.freeze_aura.decay_speed
+        else:
+            if self.freeze_aura_decay_speed > FREEZE_AURA_STARTING_DECAY_SPEED:
+                self.freeze_aura_decay_speed += FREEZE_AURA_RESTORE_ACCELERATION_RATE * dt # < 0
+            if self.freeze_aura_decay_speed < FREEZE_AURA_STARTING_DECAY_SPEED:
+                self.freeze_aura_decay_speed = FREEZE_AURA_STARTING_DECAY_SPEED
 
     def affectedBy(self, element, gauge):
         ic('dummy is affected by~', element, gauge)
--- src/genshin.py
+++ src/genshin.py
@@ -15,3 +15,36 @@ SRP = {
     GEO: (FREEZE, ELECTRO, PYRO, BURNING, HYDRO, CRYO) # shattered, clear the Freeze Aura, and applies the attack's element afterwards
 }
 # BURNING: 2U, doesn't decay, reacted by HYDRO, ELECTRO, ANEMO, GEO at the same time with underlying PYRO aura
+FREEZE_AURA_STARTING_DECAY_SPEED = 0.4 # U/s
+FREEZE_AURA_DECAY_ACCELERATION_RATE = 0.1 # U/s^2
+FREEZE_AURA_RESTORE_ACCELERATION_RATE = -0.2 # U/s^2
+
+
+class Aura:
+    def __init__(self, aura, gauge, decay_rate):
+        self.aura = aura
+        self.gauge = gauge
+        self.decay_rate = decay_rate
+        self.decay_speed = 1 / self.decay_rate
+
+    def update(self, dt):
+        if self.gauge > 0:
+            self.gauge -= self.decay_speed * dt
+        if self.gauge < 0:
+            self.gauge = 0
+
+
+class Freeze_aura():
+    def __init(self, gauge, starting_decay_speed):
+        self.aura = FREEZE
+        self.gauge = gauge
+        self.decay_rate = 1 / starting_decay_speed
+        self.decay_speed = starting_decay_speed
+
+    def update(self, dt):
+        if self.gauge > 0:
+            self.decay_speed += FREEZE_AURA_DECAY_ACCELERATION_RATE * dt
+            self.gauge -= self.decay_speed * dt
+            self.decay_rate = 1 / self.decay_speed
+        if self.gauge < 0:
+            self.gauge = 0
